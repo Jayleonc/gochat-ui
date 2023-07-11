d4fc281ecda532403a73cc867c4affac7ff37671
jayleonc <jayleonc@163.com>
2023-07-11 11:46:05 +0800

    11

--- src/api/env.tsx
+++ src/api/env.tsx
@@ -1,2 +1,2 @@
-export const ApiHost = "http://192.168.1.14:7070";
-export const SocketUrl = "ws://192.168.1.14:7090/ws";
+export const ApiHost = "http://175.178.58.198:7070";
+export const SocketUrl = "ws://175.178.58.198:7090/ws";
